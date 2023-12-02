ed608248fb1ccfd02dbb78b599d6c0f090d7bebf
nickorlabs <130688404+nickorlabs@users.noreply.github.com>
2023-12-01 20:14:51 -0600

    Update q.py

--- q.py
+++ q.py
@@ -23,6 +23,10 @@ import logging
 import threading
 import sys
 import time
+import litellm   #import litellm Should this import specific parts of liteLLM?  
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.llms import Ollama
 
 # Determine the directory of the script
 script_directory = os.path.dirname(os.path.abspath(__file__))
@@ -103,8 +107,65 @@ def stop_loading_animation(thread):
 
 # Load the AutoGen configuration from a JSON file
 
+
+# Add in LLM Selector - promptengineer48 example of this https://github.com/PromptEngineer48/LLM_Selector
+
+
+def select_best_model(user_input, models_dict):
+    llm = Ollama(model="neural-chat") #Selector Model
+
+    # Construct the prompt for the LLM
+    prompt = f"Given the user question: '{user_input}', evaluate which of the following models is most suitable: Strictly respond in 1 word only."
+    for model, description in models_dict.items():
+        prompt += f"\n- {model}: {description}"
+    # print('prompt:', prompt)
+    
+    # Send the prompt to the LLM
+    llm_response = llm(prompt)
+
+    # print("llm_response: ", llm_response)
+
+    # Parse the response to find the best model
+    # This part depends on how your LLM formats its response. You might need to adjust the parsing logic.
+    best_model = parse_llm_response(llm_response, models_dict=models_dict)
+
+    return best_model
+def parse_llm_response(response, models_dict):
+    # Convert response to lower case for case-insensitive matching
+    response_lower = response.lower()
+
+    # Initialize a dictionary to store the occurrence count of each model in the response
+    model_occurrences = {model: response_lower.count(model) for model in models_dict}
+
+    # Find the model with the highest occurrence count
+    best_model = max(model_occurrences, key=model_occurrences.get)
+
+    # If no model is mentioned or there is a tie, you might need additional logic to handle these cases
+    if model_occurrences[best_model] == 0:
+        return "neural-chat"  # Or some default model
+
+    return best_model
+
+
+models_dict = {
+    'neural-chat': 'A fine-tuned model based on Mistral with good coverage of domain and language.',
+    'mistral': 'The popular model which is able to generate coherent text and perform various natural language processing tasks.',
+    'codellama': 'A model that can use generate and discuss code and programme in python, c++, javacript and all the programming languages.',
+    'llama2-uncensored': 'Uncensored Llama 2 model. This model can give NSFW replies and can be used to generate anything which the other models are shy about. Questions on hacking, immorality, sexual and all bad contents can be asked to this model',
+    'orca-mini': 'A general-purpose model ranging from 3 billion parameters to 70 billion, suitable for entry-level hardware.',
+    'llama2-chinese': 'Llama 2 based model fine tuned to improve Chinese dialogue ability.',
+    'dolphin2.2-mistral': 'An instruct-tuned model based on Mistral. Version 2.2 is fine-tuned for improved conversation and empathy.',
+}
+
+
+
+
 try:
-    config_list_gpt4 = config_list_from_json("OAI_CONFIG_LIST.json")
+    config_list_gpt4 = config_list_from_json("OAI_CONFIG_LIST.json")  # move from OAI config list json to here in the code for now
+
+
+
+
     # 
     # Reference to ollama with autogen - https://gist.github.com/mberman84/ea207e7d9e5f8c5f6a3252883ef16df3
     # Can we adjust the OAI to look like the following:
@@ -127,19 +188,21 @@ except Exception as e:  # Leave exception handling
     print(f"Failed to load configuration: {e}")
     sys.exit(1)
 
-llm_config = {"config_list": config_list_gpt4, "cache_seed": 42}
+llm_config = {"config_list": config_list_gpt4, "cache_seed": 42}  # adjust this to below
 
-#################################################################
-# Update to something along this.  add '"cache_seed": 42'? Review this setting and adjust as needed
-#
 # llm_config_mistral={
 # "config_list": config_list_mistral,
+# "cache_seed": 42
 # }
 # 
 # llm_config_codellama={
 # "config_list": config_list_codellama,
+# "cache_seed": 42
 # }
-# Could model and user be more dynamic?
+
+
+
+
 
 
 
@@ -149,22 +212,22 @@ llm_config = {"config_list": config_list_gpt4, "cache_seed": 42}
 #
 user_proxy = UserProxyAgent(name="User_proxy", system_message="A human admin.", code_execution_config={"last_n_messages": 3, "work_dir": "./tmp"}, human_input_mode="NEVER")
 
-# Add in LLM Selector - promptengineer48 example of this https://github.com/PromptEngineer48/LLM_Selector
+
 
 
 # Update User Proxy Agent to the following, additionally possibly use nueral-chat model 
 #
-# user_proxy = autogen.UserProxyAgent(
-# name="user_proxy",
-# human_input_mode="NEVER",
-# max_consecutive_auto_reply=10,
-# is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
-# code_execution_config={"work_dir": "web"},
-# llm_config=llm_config_mistral,
-# system_message="""Reply TERMINATE if the task has been solved at full satisfaction.
-# Otherwise, reply CONTINUE, or the reason why the task is not solved yet."""
-# )
-#
+user_proxy = autogen.UserProxyAgent(
+name="user_proxy",
+human_input_mode="NEVER",
+max_consecutive_auto_reply=10,
+is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
+code_execution_config={"work_dir": "web"},
+llm_config=llm_config_mistral,
+system_message="""Reply TERMINATE if the task has been solved at full satisfaction.
+Otherwise, reply CONTINUE, or the reason why the task is not solved yet."""
+)
+
 # Update the LLM here, possibly can auto select the LLM maybe this will be a variable?
 coder = AssistantAgent(name="Coder", llm_config=llm_config)
 critic = AssistantAgent(name="Critic", system_message="Critic agent's system message here...", llm_config=llm_config)
